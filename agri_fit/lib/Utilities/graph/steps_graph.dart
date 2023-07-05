59ecdc34e2a2fc0c7c55621d791a1abddcb6e1eb
Tommy <tommytraan99@outlook.com>
2023-07-05 23:17:54 +0200

    created layout for homepage

---
+++ agri_fit/lib/Utilities/graph/steps_graph.dart
@@ -0,0 +1,113 @@
+import 'package:agri_fit/Utilities/graph/steps_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:agri_fit/screens/homePage.dart';
+import 'dart:math';
+
+class StepsGraph extends StatelessWidget {
+  final List monthlySteps;
+
+  const StepsGraph({
+    super.key,
+    required this.monthlySteps,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // initialize bar data
+    BarData myBarData = BarData(
+      janMean: monthlySteps[0], 
+      febMean: monthlySteps[1], 
+      marMean: monthlySteps[2], 
+      aprMean: monthlySteps[3], 
+      mayMean: monthlySteps[4], 
+      junMean: monthlySteps[5], 
+      julMean: monthlySteps[6], 
+      augMean: monthlySteps[7], 
+      sepMean: monthlySteps[8], 
+      octMean: monthlySteps[9], 
+      novMean: monthlySteps[10], 
+      desMean: monthlySteps[11],
+      );
+      myBarData.initializeBarData(); 
+    
+    return BarChart(
+      BarChartData(
+        minY: 0,
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+          show: true,
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, getTitlesWidget: bottomTitles))
+        ),
+        barGroups: myBarData.barData.map(
+          (data) => BarChartGroupData(
+            x: data.x,
+            barRods: [BarChartRodData(
+              toY: data.y,
+              color: const Color.fromARGB(255, 93, 155, 97),
+              width: 20,
+              borderRadius: BorderRadius.circular(3),
+              )
+              ],
+            
+            ),
+            ).toList(),
+        ),
+    );
+  }
+}
+
+Widget bottomTitles(double value, TitleMeta meta){
+  const style = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 12
+  );
+
+  Widget text;
+  switch (value.toInt()) {
+    case 1:
+      text = const Text('Jan', style: style);
+      break;
+    case 2:
+      text = const Text('Feb', style: style);
+      break;
+    case 3:
+      text = const Text('Mar', style: style);
+      break;
+    case 4:
+      text = const Text('Apr', style: style);
+      break;
+    case 5:
+      text = const Text('May', style: style);
+      break;
+    case 6:
+      text = const Text('Jun', style: style);
+      break;
+    case 7:
+      text = const Text('Jul', style: style);
+      break;
+    case 8:
+      text = const Text('Aug', style: style);
+      break;
+    case 9:
+      text = const Text('Sep', style: style);
+      break;
+    case 10:
+      text = const Text('Oct', style: style);
+      break;
+    case 11:
+      text = const Text('Nov', style: style);
+      break;
+    case 12:
+      text = const Text('Dec', style: style);
+      break;
+    default:
+      text = const Text('', style: style);
+    break;
+  }
+  return SideTitleWidget(child: text, axisSide: meta.axisSide);
+}
--- agri_fit/lib/screens/homePage.dart
+++ agri_fit/lib/screens/homePage.dart
@@ -1,13 +1,35 @@
 // ignore_for_file: file_names
 
+import 'package:agri_fit/Utilities/graph/steps_data.dart';
+import 'package:agri_fit/Utilities/graph/steps_graph.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   static const route = '/home/';
   static const routename = 'Home';
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<double> monthlySteps = [
+  6000.0,
+  8000.0,
+  15000.0,
+  16000.0,
+  17000.0,
+  15000.0,
+  18000.0,
+  14000.0,
+  14000.0,
+  15000.0,
+  10000.0,
+  8000.0
+];
 
   @override
   Widget build(BuildContext context) {
@@ -19,34 +41,74 @@ class HomePage extends StatelessWidget {
         title: const Text('AgriFit', textScaleFactor: 1.5,),
         foregroundColor: const Color.fromARGB(255, 93, 155, 97),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 15, left: 17, right: 17),
-            child: Container(
-              height: 340,
-              color: const Color.fromARGB(255, 237, 237, 237),
-              child: const Center(child: Text('Goal'),),
-            )),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 17, right: 17),
-            child: Container(
-              height: 140,
-              color: const Color.fromARGB(255, 237, 237, 237),
-              child: const Center(child: Text('Steps'),),
-            )),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 17, right: 17),
-            child: Container(
-              height: 140,
-              color: const Color.fromARGB(255, 237, 237, 237),
-              child: const Center(child: Text('Calories'),),
-            )),
-        ]),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 17, right: 17),
+        child: ListView(
+          children: [
+            const Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Monthly average steps',
+                      textScaleFactor: 1.8,
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+            Padding(
+              padding: EdgeInsets.only(top: 1),
+              child: SizedBox(
+                height: 300,
+                child: StepsGraph(
+                  monthlySteps: monthlySteps,
+                  ),
+              )),
+            const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Steps',
+                      textScaleFactor: 1.8,
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container( 
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 237, 237, 237)),
+                child: Row(
+                  children: const [
+                    Center(child: Text('12304',
+                      textScaleFactor: 2,
+                      style: TextStyle(fontWeight: FontWeight.bold)),),
+                    Center(child: Text('Steps'),),
+                  ],
+                ),
+              )),
+            const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Calories burnt',
+                      textScaleFactor: 1.8,
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container( 
+                height: 100,
+                child: const Center(child: Text('Calories'),),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromARGB(255, 237, 237, 237)),
+              )),
+          ]),
+      ),
     );
-  } //build
-
-} //ProfilePage
+  } } //ProfilePage
 
 
 void test = LineChart(
